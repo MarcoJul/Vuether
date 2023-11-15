27b1e3ac166a42bb718b2509ddb635f5f977b8d3
Marco <marco.giuliotti@gmail.com>
2023-11-15 16:53:52 +0100

    refactoring and basic style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 
-import { ref } from 'vue';
-import type { Ref } from 'vue'
+import { ref, computed } from 'vue';
+import type { Ref, ComputedRef } from 'vue';
 
 interface DaysInfo {
   conditions: string;
@@ -13,7 +13,7 @@ interface DaysInfo {
 }
 
 const key: string = import.meta.env.VITE_API_KEY;
-const days: Ref<DaysInfo[]> = ref([])
+const days: Ref<any> = ref()
 const isLoading = ref(true);
 
 async function getWeather(city: string, key: string) {
@@ -21,28 +21,50 @@ async function getWeather(city: string, key: string) {
     isLoading.value = true;
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${key}&contentType=json`);
     const data = await response.json();
-    days.value = data.days;
-    console.log(days.value);
+    console.log('data',data);
+    days.value = data;
   } catch(e) {
     console.error('There was an error', e);
   } finally {
     isLoading.value = false;
   }
 }
-
 getWeather('genoa', key);
+
+const daysShown: ComputedRef<DaysInfo[]> = computed(()=> {
+  return days.value.days.slice(0,7);
+})
 
 </script>
 
 <template>
-  <div>
-    <p v-if="!isLoading" v-for="day in days">
-      {{ day.temp }}
-      {{ day.conditions }}
-    </p>
+  <div class="plain-background">
+    <div class="container">
+      <p v-if="!isLoading" v-for="day in daysShown">
+        {{ day.datetime }}
+        {{ day.temp }}<br>
+        {{ day.conditions }}
+      </p>
+      <p v-else>Loading..</p>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+  .plain-background {
+    background-color: #E9F5FA;
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+  }
+  
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-grow: 2;
+    gap: 20px;
+    margin: 0 35px;
+  }
 </style>
